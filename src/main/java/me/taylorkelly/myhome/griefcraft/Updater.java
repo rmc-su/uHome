44895fb8f7d3c4da95c43fb3a10449d1acc10c8e
Andy "Spathi" F <github@throwaway.coldfyre.net>
2011-04-23 20:00:57 -0700

    Update url would become lib/lib/ rather than a single lib/

--- src/main/java/me/taylorkelly/myhome/griefcraft/Updater.java
+++ src/main/java/me/taylorkelly/myhome/griefcraft/Updater.java
@@ -32,7 +32,7 @@ public class Updater {
 
 	private Logger logger = Logger.getLogger("Minecraft");
 
-	private final static String UPDATE_SITE = "http://www.playminecraft.net/MyHome/lib/";
+	private final static String UPDATE_SITE = "http://www.playminecraft.net/MyHome/";
 
 	private List<UpdaterFile> needsUpdating = new ArrayList<UpdaterFile>();
 
