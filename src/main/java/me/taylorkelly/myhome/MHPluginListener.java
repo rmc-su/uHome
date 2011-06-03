e0ad3b1358882b0834b8d0e54c7b069c00dccc5a
Spathi <spathi@playminecraft.net>
2011-06-03 18:39:17 +0100

    Not hooking economy plugins if we wont use it

--- src/main/java/me/taylorkelly/myhome/MHPluginListener.java
+++ src/main/java/me/taylorkelly/myhome/MHPluginListener.java
@@ -13,27 +13,33 @@ public class MHPluginListener extends ServerListener {
 
 	public MHPluginListener(Plugin plugin) {
 		this.plugin = plugin;
-		this.Methods = new Methods();
+		if(HomeSettings.eConomyEnabled) {
+			this.Methods = new Methods();
+		}
 	}
 
 	@Override
 	public void onPluginEnable(PluginEnableEvent event) {
-		//Economy support
-		if(!this.Methods.hasMethod()){
-			if(this.Methods.setMethod(event.getPlugin())){
-				HomeEconomy.economy = this.Methods.getMethod();
-				HomeLogger.info("Connected to " + HomeEconomy.economy.getName() + " v" + HomeEconomy.economy.getVersion() + " for economy support.");
+		if(HomeSettings.eConomyEnabled) {
+			//Economy support
+			if(!this.Methods.hasMethod()){
+				if(this.Methods.setMethod(event.getPlugin())){
+					HomeEconomy.economy = this.Methods.getMethod();
+					HomeLogger.info("Connected to " + HomeEconomy.economy.getName() + " v" + HomeEconomy.economy.getVersion() + " for economy support.");
+				}
 			}
-		}	
+		}
 	}
 
 	@Override
 	public void onPluginDisable(PluginDisableEvent event) {
-		if (this.Methods != null && this.Methods.hasMethod()) {
-			Boolean check = this.Methods.checkDisabled(event.getPlugin());
-			if(check) {
-				this.Methods = null;
-				HomeLogger.info("Payment method was disabled. No longer accepting payments.");
+		if(HomeSettings.eConomyEnabled) {
+			if (this.Methods != null && this.Methods.hasMethod()) {
+				Boolean check = this.Methods.checkDisabled(event.getPlugin());
+				if(check) {
+					this.Methods = null;
+					HomeLogger.info("Payment method was disabled. No longer accepting payments.");
+				}
 			}
 		}
 	}
