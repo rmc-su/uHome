7214a35ce54ef0adf0e8696a255cba639a62282e
Spathi <spathi@playminecraft.net>
2011-07-24 21:18:00 +0100

    Removing unused import

--- src/main/java/me/taylorkelly/myhome/MyHome.java
+++ src/main/java/me/taylorkelly/myhome/MyHome.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.util.ArrayList;
 
 import me.taylorkelly.myhome.griefcraft.Updater;
-import me.taylorkelly.myhome.timers.WarmUp;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
