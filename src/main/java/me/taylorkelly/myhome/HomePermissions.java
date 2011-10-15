a6af2645f3cd4beaea526627424124fc4850f2bc
Spathi <spathi@playminecraft.net>
2011-10-15 19:04:05 +0100

    Added various * permission groups for PermBukkit

--- src/main/java/me/taylorkelly/myhome/HomePermissions.java
+++ src/main/java/me/taylorkelly/myhome/HomePermissions.java
@@ -1,5 +1,8 @@
 package me.taylorkelly.myhome;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import ru.tehkode.permissions.bukkit.*;
 import com.nijikokun.bukkit.Permissions.Permissions;
 import org.anjocaido.groupmanager.GroupManager;
@@ -76,6 +79,7 @@ public class HomePermissions {
 		registerUserPerms();
 		registerEcoPerms();
 		registerBypassPerms();
+		overallPerm();
 	}
 	
 	public static int integer(Player player, String permission, int defaultPerm) {
@@ -122,6 +126,12 @@ public class HomePermissions {
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.admin.home.any", "Admin: Teleport to any user's /home", PermissionDefault.OP));
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.admin.convert", "Admin: Convert from old homes.txt", PermissionDefault.OP));
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.admin.home.list", "Admin: See a full list of /homes", PermissionDefault.OP));
+		Map<String, Boolean> adminmap = new LinkedHashMap<String, Boolean>();
+		adminmap.put("myhome.admin.home.delete", true);
+		adminmap.put("myhome.admin.home.any", true);
+		adminmap.put("myhome.admin.convert", true);
+		adminmap.put("myhome.admin.home.list", true);
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.admin.*", "Admin: All admin commands", PermissionDefault.OP, adminmap));
 	}
 	
 	// --------------------------------------------
@@ -164,6 +174,24 @@ public class HomePermissions {
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.soc.uninvite", "Can uninvite people from your /home", PermissionDefault.TRUE));
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.soc.public", "Allow anyone to use your /home", PermissionDefault.TRUE));
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.soc.private", "Disallow anyone to use your /home", PermissionDefault.TRUE));
+		Map<String, Boolean> userbasicmap = new LinkedHashMap<String, Boolean>();
+		Map<String, Boolean> usersocmap = new LinkedHashMap<String, Boolean>();
+		Map<String, Boolean> userallmap = new LinkedHashMap<String, Boolean>();
+		userbasicmap.put("myhome.home.basic.home", true);
+		userbasicmap.put("myhome.home.basic.set", true);
+		userbasicmap.put("myhome.home.basic.delete", true);
+		usersocmap.put("myhome.home.soc.list", true);
+		usersocmap.put("myhome.home.soc.others", true);
+		usersocmap.put("myhome.home.soc.invite", true);
+		usersocmap.put("myhome.home.soc.uninvite", true);
+		usersocmap.put("myhome.home.soc.public", true);
+		usersocmap.put("myhome.home.soc.private", true);
+		userallmap.putAll(userbasicmap);
+		userallmap.putAll(usersocmap);
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.basic.*", "Basic /home commands", PermissionDefault.TRUE, userbasicmap));
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.soc.*", "Social /home commands", PermissionDefault.TRUE, usersocmap));
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.*", "All user /home commands", PermissionDefault.TRUE, userallmap));
+	
 	}
 	
 	// ------------------------------------
@@ -171,18 +199,22 @@ public class HomePermissions {
 	public static boolean setHomeFree(Player player) {
 		if(!HomeSettings.enableFreePerms) return false;
 		
-		return permission(player, "myhome.home.free.sethome", true);
+		return permission(player, "myhome.econ.free.sethome", true);
 	}
 	
 	public static boolean homeFree(Player player) {
 		if(!HomeSettings.enableFreePerms) return false;
 		
-		return permission(player, "myhome.home.free.home", true);
+		return permission(player, "myhome.econ.free.home", true);
 	}
 	
 	private static void registerEcoPerms() {
-		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.free.sethome", "Free usage of /sethome", PermissionDefault.OP));
-		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.free.home", "Free usage of /home", PermissionDefault.OP));
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.econ.free.sethome", "Free usage of /sethome", PermissionDefault.OP));
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.econ.free.home", "Free usage of /home", PermissionDefault.OP));
+		Map<String, Boolean> econmap = new LinkedHashMap<String, Boolean>();
+		econmap.put("myhome.econ.free.sethome", true);
+		econmap.put("myhome.econ.free.home", true);
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.home.free.*", "Free usage of commands", PermissionDefault.OP, econmap));
 	}
 	
 	// -----------------------------------
@@ -226,5 +258,22 @@ public class HomePermissions {
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.bypass.sethomecooldown", "Bypass: Do not have to wait for cooldown to use /sethome", PermissionDefault.OP));
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.bypass.dmgaborting", "Bypass: Do not abort /home if damaged", PermissionDefault.OP));
 		pm.addPermission(new org.bukkit.permissions.Permission("myhome.bypass.moveaborting", "Bypass: Do not abort /home if moving", PermissionDefault.OP));
+		Map<String, Boolean> bypassmap = new LinkedHashMap<String, Boolean>();
+		bypassmap.put("myhome.bypass.bedsethome", true);
+		bypassmap.put("myhome.bypass.cooldown", true);
+		bypassmap.put("myhome.bypass.warmup", true);
+		bypassmap.put("myhome.bypass.sethomecooldown", true);
+		bypassmap.put("myhome.bypass.dmgaborting", true);
+		bypassmap.put("myhome.bypass.moveaborting", true);
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.bypass.*", "Bypass all timers and restrictions", PermissionDefault.OP, bypassmap));
+	}
+	
+	public static void overallPerm() {
+		Map<String, Boolean> fullmap = null;
+		fullmap.put("myhome.econ.free.*", true);
+		fullmap.put("myhome.bypass.*", true);
+		fullmap.put("myhome.home.soc.*", true);
+		fullmap.put("myhome.home.basic.*", true);
+		pm.addPermission(new org.bukkit.permissions.Permission("myhome.*", "Full access", PermissionDefault.OP, fullmap));
 	}
 }
