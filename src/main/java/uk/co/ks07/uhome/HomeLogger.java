2336b9b1f7a58fbb783ccd0b880edb105bdfdce2
ks07 <gfield145@gmail.com>
2012-02-01 00:15:44 +0000

    Get logger via Bukkit.

--- src/main/java/uk/co/ks07/uhome/HomeLogger.java
+++ src/main/java/uk/co/ks07/uhome/HomeLogger.java
@@ -3,9 +3,11 @@ package uk.co.ks07.uhome;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+
 public class HomeLogger {
 
-    public static final Logger log = Logger.getLogger("Minecraft");
+    public static final Logger log = Bukkit.getLogger();
 
     public static void severe(String string, Exception ex) {
         log.log(Level.SEVERE, "[uHome] " + string, ex);
