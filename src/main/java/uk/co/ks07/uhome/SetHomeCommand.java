defa3011eaf0c6cf0d7c8cd7c0a152f2b54246b4
ks07 <gfield145@gmail.com>
2012-03-13 18:11:06 +0000

    Update SetHomeCommand to reflect HomeCommand.

--- src/main/java/uk/co/ks07/uhome/SetHomeCommand.java
+++ src/main/java/uk/co/ks07/uhome/SetHomeCommand.java
@@ -1,11 +1,14 @@
 package uk.co.ks07.uhome;
 
+import java.util.HashMap;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import uk.co.ks07.uhome.HomeList.ExitStatus;
 
 import uk.co.ks07.uhome.locale.LocaleManager;
+import uk.co.ks07.uhome.timers.SetHomeCoolDown;
 
 public class SetHomeCommand implements CommandExecutor {
 
@@ -29,9 +32,9 @@ public class SetHomeCommand implements CommandExecutor {
                     }
 
                     if (args.length == 1) {
-                        homeList.addHome(player, plugin, args[0], plugin.getLogger());
+                        this.setHome(player, args[0]);
                     } else if (args.length == 0) {
-                        homeList.addHome(player, plugin, uHome.DEFAULT_HOME, plugin.getLogger());
+                        this.setHome(player, uHome.DEFAULT_HOME);
                     } else {
                         return false;
                     }
@@ -40,5 +43,43 @@ public class SetHomeCommand implements CommandExecutor {
             }
         }
         return true;
+    }
+
+    private void setHome(Player player, String name) {
+        ExitStatus es = this.homeList.addHome(player, plugin, name, plugin.getLogger());
+
+        switch (es) {
+            case SUCCESS:
+                player.sendMessage(LocaleManager.getString("own.set.new"));
+                break;
+            case SUCCESS_FIRST:
+                player.sendMessage(LocaleManager.getString("own.set.first"));
+                break;
+            case SUCCESS_MOVED:
+                player.sendMessage(LocaleManager.getString("own.set.moved"));
+                break;
+            case AT_LIMIT:
+                Integer mustDelete = this.homeList.getPlayerHomeCount(player.getName()) - SuperPermsManager.getHomeLimit(player);
+                HashMap<String, String> vars = new HashMap<String, String>();
+                vars.put("DELETE", mustDelete.toString());
+                vars.put("LIMIT", Integer.toString(SuperPermsManager.getHomeLimit(player)));
+
+                player.sendMessage(LocaleManager.getString("own.set.atlimit", vars));
+                break;
+            case NEED_COOLDOWN:
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("CD_REMAINING", Integer.toString(SetHomeCoolDown.getInstance().estimateTimeLeft(player)));
+                params.put("CD_TOTAL", Integer.toString(SetHomeCoolDown.getInstance().getTimer(player)));
+
+                player.sendMessage(LocaleManager.getString("own.set.cooldown", params));
+                break;
+            case NOT_ENOUGH_MONEY:
+                HashMap<String, String> costs = new HashMap<String, String>();
+                costs.put("COST", Integer.toString(HomeConfig.setCost));
+                costs.put("CURRENCY", this.plugin.economy.currencyNamePlural());
+
+                player.sendMessage(LocaleManager.getString("econ.insufficient.set", costs));
+                break;
+        }
     }
 }
